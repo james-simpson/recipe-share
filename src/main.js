9dc79b3550aed5e32226071a5ef70d7e66475872
James Simpson <jsimpson92@hotmail.co.uk>
2018-02-20 16:25:03 +0000

    Keep scroll position when navigating between routes

--- src/main.js
+++ src/main.js
@@ -1,17 +1,18 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import Vuex from 'vuex'
+import VueScrollBehavior from 'vue-scroll-behavior'
+// VueAutosize is used for the textarea used to edit a recipe's ingredients/method
+import VueAutosize from 'vue-autosize'
 import 'vuetify/dist/vuetify.css'
 import App from './App'
 import router from './router'
 import store from './store/store'
 
-// VueAutosize is used for the textarea used to edit a recipe's ingredients/method
-import VueAutosize from 'vue-autosize'
-
 Vue.use(Vuetify)
 Vue.use(Vuex)
 Vue.use(VueAutosize)
+Vue.use(VueScrollBehavior, { router: router })
 
 Vue.config.productionTip = false
 
